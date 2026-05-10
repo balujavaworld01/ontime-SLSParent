2032d1be3e255ad38df98c05cd3426896170ddcf
balujavaworld01 <balujavaworld01@gmail.com>
2026-05-10 15:02:06 +0530

    Add files via upload
    SLSParent

---
+++ SLSParent/src/main/java/za/co/soulstice/SlsParentApplication.java
@@ -0,0 +1,12 @@
+package za.co.soulstice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlsParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlsParentApplication.class, args);
+	}
+}
---
+++ SLSParent/src/test/java/za/co/soulstice/SlsParentApplicationTests.java
@@ -0,0 +1,11 @@
+package za.co.soulstice;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SlsParentApplicationTests {
+
+	//@Test
+	public void contextLoads() {
+	}
+
+}
